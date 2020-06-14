1a864118fb8c59295ca9a9a6458853d84344e5de
ShinSuke711 <66830613+ShinSuke711@users.noreply.github.com>
2020-06-14 10:33:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+
+// Load up the discord.js library
 const Discord = require("discord.js");
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
 const client = new Discord.Client();
+
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
